1db3d0dcf5c32562fb53848a1f075699ec68ea2d
openbytes <34796404+openbytes@users.noreply.github.com>
2020-03-22 10:23:36 +0800

    Create filterID.py
    通过用户ID,过滤hadoop日志

---
+++ filterID.py
@@ -0,0 +1,22 @@
+# coding=utf8
+__data__ = '2019/12/2'
+__author__ = 'Administrator'
+
+import time
+import subprocess
+from concurrent.futures import ThreadPoolExecutor as t
+
+
+def filteID(id):
+    start = time.time()
+    command = "source /etc/profile;hadoop fs -cat /flume/data/logs/20200311/* | grep " + str(id) + " >>" + str(
+        id) + '.log'
+    r = subprocess.getstatusoutput(command)
+    end = time.time()
+    print(end - start)
+
+
+pool = t(8)
+with open('id') as f:
+    for line in f:
+        a = pool.submit(filteID, line.strip())
