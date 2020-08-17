eed94b7e3f77506902745a32f7692f14809bf4b5
openbytes <34796404+openbytes@users.noreply.github.com>
2020-08-17 10:16:08 +0800

    任意时间执行脚本,生成当月值班表. 不仅限于月初1号执行.

--- dutty_table.py
+++ dutty_table.py
@@ -1,7 +1,7 @@
 # coding=utf8
 
 import datetime
-import pymysql
+# import pymysql
 import calendar
 
 '''创建连接'''
@@ -13,14 +13,14 @@ db_config = {
     "db": 'dianyou',
     "charset": 'utf8'
 }
-conn = pymysql.connect(
-    host='172.10.4.92',
-    port=3306,
-    user='root',
-    passwd='123456',
-    db='dianyou',
-    charset='utf8'
-)
+# conn = pymysql.connect(
+#     host='172.10.4.92',
+#     port=3306,
+#     user='root',
+#     passwd='123456',
+#     db='dianyou',
+#     charset='utf8'
+# )
 
 phone_info = {
     "王维": "13128843121",
@@ -33,15 +33,15 @@ duty_people_info = ['王维', '于华云', '雷善义']
 weekdays = {0: "星期一", 1: "星期二", 2: "星期三", 3: "星期四", 4: "星期五", 5: "星期六", 6: "星期天"}
 
 
-def duty_yestoday():
-    """:arg
-    昨天值班的人
-    """
-    cursor = conn.cursor()
-    cursor.execute(
-        "select * from duty_dutytable where duty_day='{0}'".format(datetime.date.today() + datetime.timedelta(days=-1)))
-    res = cursor.fetchone()
-    return res[3]
+# def duty_yestoday():
+#     """:arg
+#     昨天值班的人
+#     """
+#     cursor = conn.cursor()
+#     cursor.execute(
+#         "select * from duty_dutytable where duty_day='{0}'".format(datetime.date.today() + datetime.timedelta(days=-1)))
+#     res = cursor.fetchone()
+#     return res[3]
 
 
 def leap_year():
@@ -59,53 +59,49 @@ def leap_year():
 
 def nums_of_days():
     "当月有几天"
-    current_year = datetime.datetime.now().year
-    current_month = datetime.datetime.now().month
-    return calendar.monthrange(current_year, current_month)[1]
+    year = datetime.datetime.now().year
+    month = datetime.datetime.now().month
+    return calendar.monthrange(year, month)[1]
+
+
+def left_days():
+    """执行脚本当前的日期距离月底剩余的天数"""
+    year = datetime.datetime.now().year
+    month = datetime.datetime.now().month
+    day = datetime.datetime.now().day
+    end = nums_of_days()
+    current_date = f"{year}-{month}-{day}"
+    # start_date = f"{year}-{month}-01"
+    end_date = f"{year}-{month}-{end}"
+    format_start_date = datetime.datetime.strptime(current_date, "%Y-%m-%d")
+    format_end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+    return (format_end_date - format_start_date).days +1
 
 
 def main():
-    count = 0
-    cursor = conn.cursor()
-    for i in range(nums_of_days()):
+    # cursor = conn.cursor()
+    for i in range(left_days()):
         # for i in range(month_day[str(datetime.datetime.now().month)]):  # 本月的天数
         # 今天的日期: datetime.date.today()
         # 距离今天几天的日期: datetime.date.today() + datetime.timedelta(days=+i)
         today = datetime.date.today() + datetime.timedelta(days=+i)  # 当天日期
-        # 当天值班的人
-        current_duty_people = duty_people_info[
-            (duty_people_info.index(duty_yestoday()) + 1 + i) % len(duty_people_info)]
-        # 当天值班是周几
-        current_duty_weekday = weekdays[datetime.date.weekday(today)]
-        SQL = "insert into duty_dutytable (duty_day,duty_week_day,duty_people,phone) values ('{0}','{1}','{2}','{3}')".format(
-            today, current_duty_weekday, current_duty_people, phone_info[current_duty_people])
-        cursor.execute(SQL)  # 执行sql
-        conn.commit()  # 提交事务
-    cursor.close()  # 游标关闭
-    conn.close()  # 连接关闭
-
-
-def third_day():
-    count = 0
-    cursor = conn.cursor()
-    for i in range(nums_of_days()):
-        # for i in range(month_day[str(datetime.datetime.now().month)]):  # 本月的天数
-        # 今天的日期: datetime.date.today()
-        # 距离今天几天的日期: datetime.date.today() + datetime.timedelta(days=+i)
-        today = datetime.date.today() + datetime.timedelta(days=+i)  # 当天日期
-        # 当天值班的人
-        current_duty_people = duty_people_info[
-            (duty_people_info.index(duty_yestoday()) + 1 + i) % len(duty_people_info)]
-        # 当天值班是周几
-        current_duty_weekday = weekdays[datetime.date.weekday(today)]
-        SQL = "insert into duty_dutytable (duty_day,duty_week_day,duty_people,phone) values ('{0}','{1}','{2}','{3}')".format(today, current_duty_weekday, current_duty_people, phone_info[current_duty_people])
-
-        cursor.execute(SQL)  # 执行sql
-        conn.commit()  # 提交事务
-    cursor.close()  # 游标关闭
-    conn.close()  # 连接关闭
-
+        # if duty_yestoday():
+        if True:
+            # 当天值班的人
+            current_duty_people = duty_people_info[
+                # (duty_people_info.index(duty_yestoday()) + 1 + i) % len(duty_people_info)]
+                (duty_people_info.index("王维") + 1 + i) % len(duty_people_info)]
+            # 当天值班是周几
+            current_duty_weekday = weekdays[datetime.date.weekday(today)]
+            SQL = "insert into duty_dutytable (duty_day,duty_week_day,duty_people,phone) values ('{0}','{1}','{2}','{3}')".format(
+                today, current_duty_weekday, current_duty_people, phone_info[current_duty_people])
+            # return  SQL
+            print(today,current_duty_people)
+            # cursor.execute(SQL)  # 执行sql
+            # conn.commit()  # 提交事务
+            # return "Error duty_yesterday 方法数据返回为空,请检查数据是否正确"
+    # cursor.close()  # 游标关闭
+    # conn.close()  # 连接关闭
 
 if __name__ == '__main__':
-    # main()
-    third_day()
+    main()
